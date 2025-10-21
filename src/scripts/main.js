8bffd8de131b110294db562369b967ff116e4956
Roman <hpp1ovh@gmail.com>
2025-10-21 23:02:17 +0300

    complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,28 @@ const food = {
   },
 };
 
+const div = document.createElement('div');
+
+div.innerHTML = '<h1>Food Tree</h1><div id="tree"></div>';
+document.body.appendChild(div);
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+    ul.appendChild(li);
+  }
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
